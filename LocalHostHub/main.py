f6d6f2d56defa419826775ddb5dca384b84fb6b3
EcomateDev <144458220+EcomateDev@users.noreply.github.com>
2023-10-28 15:49:20 +0200

    Update main.py

--- LocalHostHub/main.py
+++ LocalHostHub/main.py
@@ -12,7 +12,6 @@ import json
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/file-info':
-            # Send information about the current file to the control_panel.html page
             response = {
                 'currentFile': current_file
             }
@@ -22,7 +21,6 @@ class RequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(json.dumps(response).encode())
 
         elif self.path == '/server-status':
-            # Send information about the server status to the control_panel.html page
             response = {
                 'serverStatus': server_status
             }
@@ -31,11 +29,9 @@ class RequestHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(json.dumps(response).encode())
 
-# Variables to store information about the running file and server status
 current_file = "File not defined"
 server_status = "Server not running"
 
-# Function to run a file
 def run_file():
     global current_file
     file_path = filedialog.askopenfilename()
@@ -45,10 +41,8 @@ def run_file():
             # If it's code, run it
             subprocess.Popen(["python", file_path], shell=True)
         else:
-            # Otherwise, open the file with the default program
             subprocess.Popen(["start", file_path], shell=True)
 
-# Function to start the server
 def start_server():
     global server_status
     server_status = "Server is running"
@@ -56,31 +50,25 @@ def start_server():
     with socketserver.TCPServer(("localhost", 8000), handler) as httpd:
         httpd.serve_forever()
 
-# Start the server in a separate thread
 server_thread = threading.Thread(target=start_server)
 server_thread.daemon = True
 server_thread.start()
 
-# Create a Tkinter window
 root = tk.Tk()
 root.title("Run File and Server")
 
-# Create a button to choose a file and run it
 select_button = tk.Button(root, text="Choose a File", command=run_file)
 select_button.pack()
 
-# Function to update the display information
 def update_display():
     file_info_label.config(text=f"Running file: {os.path.basename(current_file)}")
     status_label.config(text=f"Server status: {server_status}")
     root.after(1000, update_display)
 
-# Display information
 file_info_label = tk.Label(root, text=f"Running file: {os.path.basename(current_file)}")
 file_info_label.pack()
 status_label = tk.Label(root, text=f"Server status: {server_status}")
 status_label.pack()
 
-# Start the Tkinter main loop
 root.after(1000, update_display)
 root.mainloop()
